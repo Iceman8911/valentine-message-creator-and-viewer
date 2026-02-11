0a89603d897e953250a1cfe9049bb81306806e71
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-11 09:15:10 +0100

    refactor: remove default components

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,18 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => (
+				<div class="h-screen w-screen overflow-auto">
+					<Suspense>{props.children}</Suspense>
+				</div>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
