54c982893081943cc1c25f105ba6f44b3ddea198
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-12 19:36:55 +0100

    feat(components): style BasePopover

--- src/components/ui/BasePopover.tsx
+++ src/components/ui/BasePopover.tsx
@@ -34,19 +34,23 @@ export default function BasePopover(props: BasePopoverProps) {
 			</Popover.Trigger>
 			<Popover.Portal>
 				<Popover.Content
-					class={props.class?.content || ""}
+					class={`ui-expanded:motion-opacity-in-0 ui-not-expanded:motion-opacity-out-0 max-w-2xs origin-(--kb-popover-content-transform-origin) rounded-box border border-base-300 bg-base-200 p-4 shadow ${props.class?.content || ""}`}
 					{...props.config?.content}
 				>
 					<Popover.Arrow {...props.config?.arrow} />
 					<div class="mb-1.5 flex items-baseline justify-between">
-						<Popover.Title class={props.class?.title || ""}>
+						<Popover.Title class={`font-semibold ${props.class?.title || ""}`}>
 							{props.title}
 						</Popover.Title>
-						<Popover.CloseButton class={props.class?.closeBtn || ""}>
+						<Popover.CloseButton
+							class={`btn btn-circle btn-xs ${props.class?.closeBtn || ""}`}
+						>
 							<XIcon />
 						</Popover.CloseButton>
 					</div>
-					<Popover.Description class={props.class?.description || ""}>
+					<Popover.Description
+						class={`text-sm ${props.class?.description || ""}`}
+					>
 						{props.description}
 					</Popover.Description>
 				</Popover.Content>
