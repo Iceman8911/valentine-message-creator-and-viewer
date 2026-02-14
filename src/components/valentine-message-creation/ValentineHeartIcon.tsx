96ee72f23d7f70acf4a8233759f1bc100bb14559
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-14 14:10:51 +0100

    feat: add heart pulse anim

--- src/components/valentine-message-creation/ValentineHeartIcon.tsx
+++ src/components/valentine-message-creation/ValentineHeartIcon.tsx
@@ -2,5 +2,9 @@ import clsx from "clsx/lite";
 import { HeartIcon } from "lucide-solid";
 
 export default function ValentineHeartIcon(props: { class?: string }) {
-	return <HeartIcon class={clsx("fill-primary", props.class)} />;
+	return (
+		<HeartIcon
+			class={clsx("fill-primary motion-safe:animate-pulse", props.class)}
+		/>
+	);
 }
